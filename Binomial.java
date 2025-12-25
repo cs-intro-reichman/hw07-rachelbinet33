91ff74910ee1a3a1578614421542e200d3686cd3
רחלי בינט <rachelbinet@MacBook-Air-sl-rhly.local>
2025-12-25 22:24:38 +0200

    fdgb

--- Binomial.java
+++ Binomial.java
@@ -18,9 +18,9 @@ public class Binomial {
 		}
 	
 	// Computes the Binomial function, efficiently
-	public static int binomial(int n, int k) {
+	public static long binomial(int n, int k) {
 		if (k > n) return 0;
-		int[][] memo = new int[n + 1][k + 1];
+		long[][] memo = new long[n + 1][k + 1];
 		for (int i = 0; i <= n; i++) {
             for (int j = 0; j <= k; j++) {
                 memo[i][j] = -1;
@@ -29,7 +29,7 @@ public class Binomial {
 		return binomial(n, k, memo);
 	}
 
-	private static int binomial(int n, int k, int[][] memo) {
+	private static long binomial(int n, int k, long[][] memo) {
 		if (memo[n][k] != -1) {
 			return memo[n][k];
 		}
