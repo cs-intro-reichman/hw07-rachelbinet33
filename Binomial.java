fdb0141e37f4936acd8a2bb6684e2fd0906b3831
רחלי בינט <rachelbinet@MacBook-Air-sl-rhly.local>
2025-12-25 22:11:12 +0200

    fvvd

--- Binomial.java
+++ Binomial.java
@@ -9,14 +9,14 @@ public class Binomial {
 		// Testing the optimized binomial implementation:
 		 System.out.println(binomial(Integer.parseInt(args[0]), Integer.parseInt(args[1])));
 	}
-
+]f
 	// Computes the Binomial function, basic version.
-	/*public static int binomial1(int n, int k) {
+	public static int binomial1(int n, int k) {
 		if (k > n) return 0;
 		if (k == 0 || n == 0) return 1;
 		return binomial1(n - 1, k) + binomial1(n - 1, k - 1);
 		}
-	*/
+	
 	// Computes the Binomial function, efficiently
 	public static int binomial(int n, int k) {
 		if (k > n) return 0;
